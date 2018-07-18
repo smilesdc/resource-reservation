a0b09019d9d22ac045402631a63af53940aa1966
Smile <smilesdcru@gmail.com>
2018-07-18 19:38:41 +0300

    Init

---
+++ src/test/java/me/smilesdc/reservation/resourcereservation/ResourceReservationApplicationTests.java
@@ -0,0 +1,16 @@
+package me.smilesdc.reservation.resourcereservation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResourceReservationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
